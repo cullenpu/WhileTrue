da64f4a8fd21b2c2a3a78fbda753656505e1501e
Cullen Pu <cullenpu@gmail.com>
2021-11-18 16:40:25 -0500

    Center text on generate copy page

--- src/app/src/pages/GenerateCopy.tsx
+++ src/app/src/pages/GenerateCopy.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import { Center, Text } from '@chakra-ui/react';
 
 export const GenerateCopy = () => {
-  return;
-  <div>
-    <Center>
-      <Text fontSize="5xl">Generate Copy</Text>
-    </Center>
-  </div>;
+  return (
+    <div>
+      <Center>
+        <Text fontSize="5xl">Generate Copy</Text>
+      </Center>
+    </div>
+  );
 };
