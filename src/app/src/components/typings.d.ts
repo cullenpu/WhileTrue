cf623a2ae40f6ca37798d65061c6dc00fb069d6e
Shuaitian Chen <shuaitian.chen@mail.utoronto.ca>
2021-11-22 05:41:54 -0500

    refine generate content with chakra-ui

--- src/app/src/components/typings.d.ts
+++ src/app/src/components/typings.d.ts
@@ -2,3 +2,15 @@ export interface ContentType {
   contentTitle: string;
   contentText: string;
 }
+
+export interface LabelType {
+  labelText: string;
+}
+
+export interface GenerateButton {
+  buttonText: string;
+}
+
+export interface GenerateSearch {
+  barText: string;
+}
